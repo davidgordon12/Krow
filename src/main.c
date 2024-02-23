8c1b3eb6080daebe9bdf9e15f96d22db88ca46ce
David Gordon <davidgordon12@live.ca>
2024-02-23 12:45:06 -0500

    Implement deleting and newlines

--- src/main.c
+++ src/main.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 #include "raylib.h"
 
+#define TITLE "Krow"
+#define WIDTH 1200
+#define HEIGHT 800
+
+#define FONT GetFontDefault()
 #define FONT_SIZE 40
 #define SPACING 33
 
 #define KROWGRAY   CLITERAL(Color){ 35, 35, 35, 86 }
 
-#define SHIFT 340
+#define SHIFT      340
 #define BACKSPACE  259
-#define SPACE 320
-#define ENTER 257
+#define SPACE      320
+#define ENTER      257
 #define FULLSCREEN 300
+#define TAB        258
+
+static void DrawBuffer(int* buf, int len);
+static int GetLineLength(int* buf);
 
 int main(void)
 {
     int width = 0;
     int posX = 0;
     int posY = 0;
-    InitWindow(1200, 800, "Krow");
+    int len = 0;
+    InitWindow(WIDTH, HEIGHT, TITLE);
     Font f = GetFontDefault();
     ClearBackground(KROWGRAY);
-
+    int buffer[1024000];
     while (!WindowShouldClose())
     {
         EnableEventWaiting();
@@ -30,19 +40,39 @@ int main(void)
 
         if(charKey != 0) printf("|%d| ", charKey);
 
-        if(charKey != 0 && charKey != SHIFT && charKey != BACKSPACE && charKey != ENTER) {
+        if(charKey >= 32 && charKey <= 255) {
             Vector2 pos = { posX, posY };
             DrawTextCodepoint(f, charKey, pos, FONT_SIZE, PURPLE);
             posX += SPACING;
+            buffer[len] = charKey;
             width++;
-        } else if(charKey == BACKSPACE && posX >= FONT_SIZE) {
-            ClearBackground(KROWGRAY);
-        } else if(charKey == ENTER) {
-            posX = 0;
-            posY += FONT_SIZE;
-        } else if(charKey == FULLSCREEN) {
-            // TODO: Get this working
-            // SetWindowSize(GetMonitorWidth(0), GetMonitorHeight(0));
+            len++;
+        } else {
+            switch (charKey) {
+            case BACKSPACE:
+                if(posX > 0) {
+                    ClearBackground(KROWGRAY);
+                    len--;
+                    DrawBuffer(buffer, len);
+                    posX -= SPACING;
+                } else if(posX == 0 && posY >= FONT_SIZE) {
+                    ClearBackground(KROWGRAY);
+                    len--;
+                    DrawBuffer(buffer, len);
+                    posY -= FONT_SIZE;
+                    // posX = amount between the last two newLines, or if its the first line,
+                    // 0 -> first newLine
+                    // len should be set according to the new posX
+                    // this way we can delete things in previous lines
+                }
+                break;
+            case ENTER:
+                posX = 0;
+                posY += FONT_SIZE;
+                buffer[len] = '\r';
+                len++;
+                break;
+            }
         }
 
         EndDrawing();
@@ -50,5 +80,23 @@ int main(void)
 
     CloseWindow();
 
+    return 0;
+}
+
+void DrawBuffer(int* buf, int len) {
+    Vector2 pos = { 0, 0 };
+    for(int i = 0; i < len; ++i) {
+        if(buf[i] >= 32 && buf[i] <= 255 && buf[i] != '\n') {
+            DrawTextCodepoint(FONT, buf[i], pos, FONT_SIZE, PURPLE);
+            pos.x += SPACING;
+        }
+        else if(buf[i] == '\r') {
+            pos.x = 0;
+            pos.y += FONT_SIZE;
+        }
+    }
+}
+
+int GetLineLength(int* buf) {
     return 0;
 }
