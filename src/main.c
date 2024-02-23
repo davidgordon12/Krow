886cbbe7219a4d394b2f56c1bbad92c1e3622e70
David Gordon <davidgordon12@live.ca>
2024-02-23 00:50:20 -0500

    Simple text rendering

--- src/main.c
+++ src/main.c
@@ -1,14 +1,50 @@
+#include <stdio.h>
 #include "raylib.h"
+
+#define FONT_SIZE 40
+#define SPACING 33
+
+#define KROWGRAY   CLITERAL(Color){ 35, 35, 35, 86 }
+
+#define SHIFT 340
+#define BACKSPACE  259
+#define SPACE 320
+#define ENTER 257
+#define FULLSCREEN 300
 
 int main(void)
 {
-    InitWindow(800, 450, "Super Combo Fighter I");
+    int width = 0;
+    int posX = 0;
+    int posY = 0;
+    InitWindow(1200, 800, "Krow");
+    Font f = GetFontDefault();
+    ClearBackground(KROWGRAY);
 
     while (!WindowShouldClose())
     {
+        EnableEventWaiting();
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        int charKey = GetKeyPressed();
+
+        if(charKey != 0) printf("|%d| ", charKey);
+
+        if(charKey != 0 && charKey != SHIFT && charKey != BACKSPACE && charKey != ENTER) {
+            Vector2 pos = { posX, posY };
+            DrawTextCodepoint(f, charKey, pos, FONT_SIZE, PURPLE);
+            posX += SPACING;
+            width++;
+        } else if(charKey == BACKSPACE && posX >= FONT_SIZE) {
+            ClearBackground(KROWGRAY);
+        } else if(charKey == ENTER) {
+            posX = 0;
+            posY += FONT_SIZE;
+        } else if(charKey == FULLSCREEN) {
+            // TODO: Get this working
+            // SetWindowSize(GetMonitorWidth(0), GetMonitorHeight(0));
+        }
+
         EndDrawing();
     }
 
